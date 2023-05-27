e987f4f378e69ab6e870e06de9e4f4ae9a678893
LaoDie1 <1693619798@qq.com>
2023-05-27 22:56:21 +0800

    添加图标

--- addons/excel_reader/plugin.gd
+++ addons/excel_reader/plugin.gd
@@ -6,9 +6,6 @@ func _enter_tree():
 	# Initialization of the plugin goes here.
 	pass
 	
-	print("example:")
-	print('var excel = ExcelFile.open_file("C:\\Users\\z\\Desktop\\role_data.xlsx")')
-	
 
 
 func _exit_tree():
