8ff7727e502823407a57f3ef95a6b7a56953f818
LaoDie1 <1693619798@qq.com>
2023-06-20 22:51:12 +0800

    更新添加根据 rid 获取 sheet 数据路径

--- addons/excel_reader/src/workbook.gd
+++ addons/excel_reader/src/workbook.gd
@@ -15,8 +15,11 @@ var xml_data : ExcelXMLData:
 
 var _zip_reader : ZIPReader
 var _sheets : Dictionary = {}
-var _sheet_files : Array[String] = []
 var _data_value_list : Array = []
+var _sheet_data_list : Array[Dictionary] = []
+
+var _rels : ExcelXMLData  # rid data
+var _rid_to_path_map : Dictionary = {}
 
 
 #============================================================
@@ -24,16 +27,29 @@ var _data_value_list : Array = []
 #============================================================
 func _init(zip_reader: ZIPReader):
 	self._zip_reader = zip_reader
-	var workbook_path = "xl/workbook.xml"
-	self.xml_data = ExcelXMLData.new(_zip_reader, workbook_path)
+	self.xml_data = ExcelXMLData.new(_zip_reader, "xl/workbook.xml")
+	self._rels = ExcelXMLData.new(_zip_reader, "xl/_rels/workbook.xml.rels")
 	
-	# 表单文件列表
-	for file in _zip_reader.get_files():
-		if file.begins_with("xl/worksheets/") and file.ends_with(".xml"):
-			_sheet_files.append(file)
-	self._sheet_files.erase("xl/worksheets/")
+	# RID file path
+	for child in _rels.get_root().get_children():
+		var id = child.get_attr("Id")
+		var target_path = child.get_attr("Target") # Files in the xl directory
+		self._rid_to_path_map[id] = target_path
 	
-	# 获取值列表，string 类型单元格数据的缓存
+	# Sheets 
+	var sheets = xml_data.get_root().get_first_node("sheets")
+	var rid : String
+	var sheet_name : String
+	for child in sheets.get_children():
+		rid = child.get_attr("r:id")
+		sheet_name = child.get_attr("name")
+		_sheet_data_list.append({
+			"rid": rid,
+			"sheet_name": sheet_name,
+			"path": _rid_to_path_map[rid],
+		})
+	
+	# 获取值列表，string 类型单元格数据的缓存（共享的字符串）
 	var sharedStrings = ExcelXMLData.new(_zip_reader, "xl/sharedStrings.xml")
 	for si_node in sharedStrings.get_root().get_children():
 		if si_node.get_child_count() > 0:
@@ -50,40 +66,45 @@ func _to_string():
 #  自定义
 #============================================================
 func _create_sheet(xml_path: String) -> ExcelSheet:
-	return ExcelSheet.new(_zip_reader, xml_path, _data_value_list)
+	return ExcelSheet.new(_zip_reader, "xl".path_join(xml_path), _data_value_list)
 
 
 func get_sheet_files() -> Array[String]:
-	return _sheet_files
+	return Array(_sheet_data_list.map(func(item): 
+		return item["path"]
+	), TYPE_STRING, &"", null)
 
 
 func get_sheet_name_list() -> Array[String]:
-	return Array(xml_data.get_first_node("workbook/_sheets").get_children().map(
-			func(item: ExcelXMLNode): return item.get_attr("name")
-		)
-		, TYPE_STRING
-		, ""
-		, null
-	)
+	return Array(_sheet_data_list.map(func(item): 
+		return item["sheet_name"]
+	), TYPE_STRING, &"", null)
 
 
 func get_sheets() -> Array[ExcelSheet]:
 	if _sheets.is_empty():
-		for xml_path in _sheet_files:
+		for xml_path in get_sheet_files():
 			_sheets[xml_path] = _create_sheet(xml_path)
 	return Array(_sheets.values(), TYPE_OBJECT, "RefCounted", ExcelSheet)
 
 
-func get_sheet(idx_or_name) -> ExcelSheet:
-	var xml_path : String = get_sheet_files()[idx_or_name] \
-		if idx_or_name is int \
-		else ("xl/worksheets/" + idx_or_name)
+func get_path_by_sheet_name(sheet_name: String) -> String:
+	for data in _sheet_data_list:
+		if data["sheet_name"] == sheet_name:
+			return data["path"]
+	return ""
+
+
+func get_sheet(idx_or_sheet_name) -> ExcelSheet:
+	var xml_path : String = get_sheet_files()[idx_or_sheet_name] \
+		if idx_or_sheet_name is int \
+		else get_path_by_sheet_name(idx_or_sheet_name)
 	
 	if not xml_path.ends_with(".xml"):
 		xml_path += ".xml"
 	
 	# 没有这个 sheet 路径
-	if not _sheet_files.has(xml_path):
+	if not get_sheet_files().has(xml_path):
 		printerr("没有这个文件：", xml_path)
 		return null
 	
--- addons/excel_reader/test.gd
+++ addons/excel_reader/test.gd
@@ -9,19 +9,25 @@
 extends EditorScript
 
 
-var excel = ExcelFile.open_file("C:\\Users\\Administrator\\Downloads\\user.xlsx")
+var excel = ExcelFile.open_file("C:\\Users\\z\\Desktop\\example.xlsx")
 
 
 func _run():
 	var workbook = excel.get_workbook()
-	var sheet = workbook.get_sheet(0) as ExcelSheet
-#	var sheet = workbook.get_sheet("sheet1") as ExcelSheet
+	print(workbook.get_sheet_files())
+	print(workbook.get_sheet_name_list())
 	
+	
+	var sheet = workbook.get_sheet(0) as ExcelSheet
 	var table_data = sheet.get_table_data()
+	print(JSON.stringify(table_data, "\t"))
+	
+	return
+	
+	
 	for row in table_data:
 		var column_data = table_data[row]
 		for column in column_data:
 			print(column_data[column])
 	
-	print(JSON.stringify(table_data, "\t"))
 
